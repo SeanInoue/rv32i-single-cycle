15bbcc69b7a02f47cfb9c2438f88e96e7ea8635b
SeanInoue <s.i.pewpew.79@gmail.com>
2026-09-20 17:19:33 -0400

    fix formatting style with verible

--- rtl/modules/alu.sv
+++ rtl/modules/alu.sv
@@ -1,28 +1,28 @@
 import riscv_pkg::*;
 
 module alu (
-    input  logic [XLEN-1:0]       alu_in1,
-    input  logic [XLEN-1:0]       alu_in2,
-    input  alu_control_t          alu_control,
+    input logic         [XLEN-1:0] alu_in1,
+    input logic         [XLEN-1:0] alu_in2,
+    input alu_control_t            alu_control,
 
-    output logic [XLEN-1:0]       alu_out,
-    output logic                  zero
+    output logic [XLEN-1:0] alu_out,
+    output logic            zero
 );
 
-    localparam int SHIFT_BITS = $clog2(XLEN);
+  localparam int ShiftBits = $clog2(XLEN);
 
   always_comb begin
     unique case (alu_control)
       ALU_ADD:    alu_out = alu_in1 + alu_in2;
-      ALU_SUB:    alu_out = alu_in1 + ~alu_in2 + (XLEN)'b1;
+      ALU_SUB:    alu_out = alu_in1 + ~alu_in2 + XLEN'(1);
       ALU_AND:    alu_out = alu_in1 & alu_in2;
       ALU_OR:     alu_out = alu_in1 | alu_in2;
       ALU_XOR:    alu_out = alu_in1 ^ alu_in2;
       ALU_SLT:    alu_out = XLEN'($signed(alu_in1) < $signed(alu_in2));
       ALU_SLTU:   alu_out = XLEN'(alu_in1 < alu_in2);
-      ALU_SLL:    alu_out = alu_in1 << alu_in2[SHIFT_BITS-1:0];
-      ALU_SRL:    alu_out = alu_in1 >> alu_in2[SHIFT_BITS-1:0];
-      ALU_SRA:    alu_out = $signed(alu_in1) >>> alu_in2[SHIFT_BITS-1:0];
+      ALU_SLL:    alu_out = alu_in1 << alu_in2[ShiftBits-1:0];
+      ALU_SRL:    alu_out = alu_in1 >> alu_in2[ShiftBits-1:0];
+      ALU_SRA:    alu_out = $signed(alu_in1) >>> alu_in2[ShiftBits-1:0];
       ALU_PASS_B: alu_out = alu_in2;
       default:    alu_out = '0;
     endcase
